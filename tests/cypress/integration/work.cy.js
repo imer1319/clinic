d34e942c645773dd44f5b6975b1bc7f36dd31112
imer1319 <imer1319nd@gmail.com>
2022-12-07 16:09:53 -0400

    cypress add

--- tests/cypress/integration/work.cy.js
+++ tests/cypress/integration/work.cy.js
@@ -5,11 +5,13 @@ describe('Citas medicas', () => {
     cy.get('#username').type('Administrador')
     cy.get('#password').type('123123')
     cy.get('.btn').click();
+    // Listado de citas medicas
+    cy.get('[data-cy=cita]').click();
+    cy.get('[data-cy=index-cita]').click();
 
     // Crear cita medica
-    cy.get('[data-cy=cita]').click();
     cy.get('[data-cy=create-cita]').click();
-    cy.get(':nth-child(2) > .fc-bg > table > tbody > tr > .fc-thu').click()
+    cy.get(':nth-child(2) > .fc-content-skeleton > table > thead > tr > .fc-thu').click()
     cy.get('#time_start').invoke('val', '11:00').trigger('change')
     cy.get('#time_end').invoke('val', '13:00').trigger('change')
     cy.get('input[type=color]')
@@ -22,7 +24,19 @@ describe('Citas medicas', () => {
     cy.get('[data-cy="subServ_1"]').check()
     cy.get('#observations').type("No tiene observaciones esta cita medica")
     cy.get('#addCita').click();
+    // Update cita medica
     cy.get('.fc-content').click()
+    cy.get('input[type=color]')
+    .invoke('val', '#ffdd00')
+    .trigger('change')
+    cy.get('#observations').clear().type("No tiene observaciones esta cita medica editado")
+    cy.get('#updateCita').click()
+    // Delete cita medica
+    cy.get('.fc-content').click()
+    cy.get('#destroyCita').click()
+    // Volver al listdo de citas medicas
+    cy.get('[data-cy=index-cita]').click();
+
   })
 
 })
